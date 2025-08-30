893dacb0ca003388b108a2954d6c46cfa586bcbd
JeroGaviiria <jeronimo.gaviriar@autonoma.edu.co>
2025-08-29 19:10:29 -0500

    Header Linio

--- components/organisms/HeaderComponent.tsx
+++ components/organisms/HeaderComponent.tsx
@@ -1,81 +1,83 @@
-
 export default function HeaderComponent() {
   return (
-    <header className="w-full flex flex-col bg-[#131921]">
-      <section className="w-full max-w-[1500px] mx-auto flex items-center">
-        <section className="w-full">
-          <picture>
+
+    <header className="w-full">
+      {/* Barra superior blanca */}
+      <div className="w-full bg-white border-b border-gray-200">
+        <div className="max-w-[1200px] flex items-center gap-8 h-8 px-4">
+          <img
+            src="https://images.falabella.com/v3/assets/blt7c5c2f2f888a7cc3/blt422ee1291fbab2a2/marketplace-disabled.svg"
+            alt="Falabella"
+            className="h-3.5"
+          />
+          <img
+            src="https://images.falabella.com/v3/assets/blt7c5c2f2f888a7cc3/blt55f993da75fa018d/homecenter-disabled.svg"
+            alt="Homecenter"
+            className="h-2.5"
+          />
+          <img
+            src="https://images.falabella.com/v3/assets/blt088e6fffbba20f16/blta5a44443cc0e501c/63b6ff659233961262fbd64c/logo-linio.svg"
+            alt="Linio"
+            className="h-8"
+          />
+        </div>
+      </div>
+
+      <div className="w-full bg-[#C300A2]">
+        <div className="max-w-[1200px]  flex items-center h-16 px-4">
+
+          {/* Logo */}
+          <div className="mr-15">
             <img
-              src="https://images.falabella.com/v3/assets/blt088e6fffbba20f16/bltf580cd4c16460848/64feccbe1d03ade334df3b16/linio.svg" 
-              alt="Icono de amazon"
-              title="Amazon es el mejor y mas caro"
-              height="50"
-              width="100"
+              src="https://images.falabella.com/v3/assets/blt088e6fffbba20f16/bltf580cd4c16460848/64feccbe1d03ade334df3b16/linio.svg"
+              alt="Linio"
+              className="h-10"
             />
-          </picture>
-        </section>
-        <section className="w-[100px] flex items-center gap-[10px]">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><circle cx="12" cy="9" r="2.5" fill="#fff"/><path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M12 20.5c0 0 -6 -7 -6 -11.5c0 -3.31 2.69 -6 6 -6c3.31 0 6 2.69 6 6c0 4.5 -6 11.5 -6 11.5Z"/></svg>
-          <div className="flex flex-col text-white">
-            <span className="text-[12px] font-semibold">Enviar a</span>
-            <span className="text-[14px] font-bold">Colombia</span>
           </div>
-        </section>
-        <section className="w-[calc(100%-610px)] h-10 flex items-center">
-          <select className="h-full w-[100px] bg-[#b3b3b3]" name="" id="">
-            <option value="">Todos</option>
-            {/* @for (item of listItems(); track item.id) {
-              <option [value]="item.id"></option>
-            } */}
-          </select>
-          <input className="h-full w-[calc(100-140px)] p-2.5 border-none outline-0 bg-white" type="text" />
-          <div className="h-full w-[40px] bg-[#febd69] flex justify-center items-center rounded-r-[5px]">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"><path d="M10.76 13.24c-2.34 -2.34 -2.34 -6.14 0 -8.49c2.34 -2.34 6.14 -2.34 8.49 0c2.34 2.34 2.34 6.14 0 8.49c-2.34 2.34 -6.14 2.34 -8.49 0Z"/><path d="M10.5 13.5l-7.5 7.5"/></g></svg>
+
+          {/* Categorías */}
+          <button className="flex items-center gap-2 px-4 py-1.5 border border-white text-white font-semibold rounded-full bg-[#C300A2] hover:bg-[#a00085] transition">
+            Categorías
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+            </svg>
+          </button>
+
+
+          {/* Barra de búsqueda */}
+          <div className="flex items-center w-full max-w-lg bg-white rounded-full overflow-hidden ml-5">
+            <input
+              type="text"
+              placeholder="Buscar en Linio"
+              className="flex-1 px-4 py-3.5 text-sm text-gray-2000 outline-none border-none"
+            />
+            <button className="bg-[#3b4552] w-9 h-9 flex items-center justify-center rounded-full mr-1">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1110.5 3a7.5 7.5 0 016.15 13.65z" />
+              </svg>
+            </button>
           </div>
-        </section>
-        <section className="header__superior--flag">
-          <span>
-            ES
-          </span>
-          {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M12 15l-5 -5M12 15l5 -5"/></svg> */}
-        </section>
-        <section className="header__superior--auth">
-          <div>
-            <span>Hola, identificate</span>
-            <span>Cuenta y listas</span>
-          </div>
-          <div>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M12 15l-5 -5M12 15l5 -5"/></svg> */}
-          </div>
-        </section>
-        <section className="header__superior--refunds">
-          <span>Devoluciones</span>
-          <span>y pedidos</span>
-        </section>
-        <section className="header__superior--cart">
-          <div>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><g fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"><path d="M9 7h11c0.55 0 1 0.45 1 1v11c0 0.55 -0.45 1 -1 1h-16c-0.55 0 -1 -0.45 -1 -1v-11c0 -0.55 0.45 -1 1 -1Z"/><path d="M9 7v-3c0 -0.55 0.45 -1 1 -1h4c0.55 0 1 0.45 1 1v3"/></g></svg> */}
-          </div>
-          <div>
-            <span>Carrito</span>
-          </div>
-        </section>
-      </section>
-      <section className="header__inferior">
-        <section>
-          <p>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#fff" stroke-dasharray="8" stroke-dashoffset="8" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"><path d="M12 5h6M12 5h-6" stroke-dashoffset="0"/><path d="M12 10h6M12 10h-6" stroke-dashoffset="0"/><path d="M12 15h6M12 15h-6" stroke-dashoffset="0"/><path d="M12 20h6M12 20h-6" stroke-dashoffset="0"/></g></svg> */}
-            Todo
-          </p>
-          <p>Prime</p>
-          <p>Prime video</p>
-          <p>Ofertas del Día</p>
-          <p>Listas</p>
-          <p>Servicio al Cliente</p>
-          <p>Tarjetas de Regalo</p>
-          <p>Vender</p>
-        </section>
-      </section>
+
+
+          {/* Opciones de usuario */}
+          <nav className="flex items-center gap-4 text-white text-sm font-medium ml-4">
+            <a href="#" className="hover:underline">Hola, Inicia sesión</a>
+            <a href="#" className="hover:underline">Mis compras</a>
+            <span className="border-r border-white h-5"></span>
+            <a href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+              </svg>
+            </a>
+            <a href="#" className="relative">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2 9h12l2-9M10 21a2 2 0 104 0" />
+              </svg>
+              <span className="absolute -top-1 -right-2 bg-white text-[#C300A2] text-xs font-bold rounded-full px-1">0</span>
+            </a>
+          </nav>
+        </div>
+      </div>
     </header>
   )
 }
