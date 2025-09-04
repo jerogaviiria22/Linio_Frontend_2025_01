d29ddf2ade1522c9a3b421b41bb4d615e0830a54
clinaresm <xdcami2@gmail.com>
2025-09-04 09:11:05 -0500

    Carrousel implemented

--- components/organisms/LinioMainCarrousel.tsx
+++ components/organisms/LinioMainCarrousel.tsx
@@ -3,9 +3,12 @@ import { useEffect, useRef, useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const images = [
-  'https://cdn.pixabay.com/photo/2016/11/22/22/39/golf-1850984_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/03/27/12/50/flower-2178507_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/03/27/13/55/controller-2178879_1280.jpg'
+  'https://images.falabella.com/v3/assets/bltf612eb601c634386/blt4ed00bc6979257f1/6807c7f7e8bf0d0fbef05501/Vitrina_Dk_Linio_Celulares.webp',
+  'https://images.falabella.com/v3/assets/bltf612eb601c634386/blt633564ce1ef5a31c/6807c808da898c20b0f87d9d/Vitrina_Dk_Linio_Infantil.webp',
+  'https://images.falabella.com/v3/assets/bltf612eb601c634386/blt714214784ac3a2da/6807c808e41b1f12e6bb95c6/Vitrina_Dk_Linio_Motos.webp',
+  'https://images.falabella.com/v3/assets/bltf612eb601c634386/blt03fe45d47b6f5071/6807c7f877cfbfc1564093a0/Vitrina_Dk_Linio_Computo.webp',
+  'https://images.falabella.com/v3/assets/bltf612eb601c634386/blt03ddd50684b1d620/6807c808850b441f61b11a1c/Vitrina_Dk_Linio_Mascotas.webp',
+  'https://images.falabella.com/v3/assets/bltf612eb601c634386/blt6d38d761db0d7757/6807c7f83ed107ad6bd03454/Vitrina_Dk_Linio_Herramientas.webp'
 ];
 
 const ImageCarousel: React.FC = () => {
@@ -31,31 +34,48 @@ const ImageCarousel: React.FC = () => {
   }, [currentIndex]);
 
   return (
-    <h1>Acá va el Carrousel</h1>
-    // <div className="relative w-full h-[50vh] overflow-hidden">
-    //   {/* Fondo con transición */}
-    //   <div
-    //     className="w-full h-full bg-cover bg-center transition-all duration-700"
-    //     style={{ backgroundImage: `url(${images[currentIndex]})` }}
-    //   />
+     <div className="w-full max-w-[1920px] mx-auto relative h-[460px] overflow-hidden rounded-lg">
+      <div
+        className="w-full h-full bg-cover bg-center transition-all duration-700 rounded-lg"
+        style={{ backgroundImage: `url(${images[currentIndex]})` }}
+      />
+      
+      {/* Botones de navegación del carrusel */}
+      <div className="absolute inset-y-0 left-0 flex items-center">
+        <button 
+          onClick={prevSlide}
+          className="bg-white bg-opacity-60 hover:bg-opacity-90 p-3 ml-2 rounded-full shadow-lg transition"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+          </svg>
+        </button>
+      </div>
 
-    //   {/* Controles */}
-    //   <button
-    //     onClick={prevSlide}
-    //     className="absolute left-4 top-1/2 -translate-y-1/2 bg-white bg-opacity-60 hover:bg-opacity-90 p-2 rounded-full shadow z-10"
-    //   >
-    //     <FaChevronLeft />
-    //   </button>
-    //   <button
-    //     onClick={nextSlide}
-    //     className="absolute right-4 top-1/2 -translate-y-1/2 bg-white bg-opacity-60 hover:bg-opacity-90 p-2 rounded-full shadow z-10"
-    //   >
-    //     <FaChevronRight />
-    //   </button>
+      <div className="absolute inset-y-0 right-0 flex items-center">
+        <button 
+          onClick={nextSlide}
+          className="bg-white bg-opacity-60 hover:bg-opacity-90 p-3 mr-2 rounded-full shadow-lg transition"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+          </svg>
+        </button>
+      </div>
 
-    //   {/* Sombras para mejorar legibilidad si hay contenido encima */}
-    //   <div className="absolute inset-0 bg-gradient-to-r from-white/50 via-transparent to-white/50 pointer-events-none" />
-    // </div>
+      {/* Indicadores de navegación */}
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {images.map((_, idx) => (
+          <div
+            key={idx}
+            className={`w-2.5 h-2.5 rounded-full cursor-pointer transition-all duration-300 ${
+              currentIndex === idx ? 'bg-white scale-125' : 'bg-gray-400'
+            }`}
+            onClick={() => setCurrentIndex(idx)}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
