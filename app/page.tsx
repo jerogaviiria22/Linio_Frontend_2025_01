6db96ac3111bfbb85f3e76e85f416af546e0c860
clinaresm <xdcami2@gmail.com>
2025-09-04 09:53:53 -0500

    LastSeen category completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import LinioMainCarrousel from "@/components/organisms/LinioMainCarrousel";
+import CategoriesLinio from "@/components/organisms/CategoriesLinio";
 import Image from "next/image";
+import LastSeen from "@/components/organisms/LastSeenLinio";
 
 export default function Home() {
   return (
-    <LinioMainCarrousel />
+    <><LinioMainCarrousel /><CategoriesLinio /> <LastSeen /> </>
   );
 }
