8d854206fed3b5d54a803735740218b015594158
clinaresm <xdcami2@gmail.com>
2025-09-04 17:36:08 -0500

    RenewYourHome completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import CategoriesLinio from "@/components/organisms/CategoriesLinio";
 import Image from "next/image";
 import LastSeen from "@/components/organisms/LastSeenLinio";
 import DiscountsTeconology from "@/components/organisms/DiscountsTecnologyLinio";
+import DontMissIt from "@/components/organisms/DontMissItLinio";
+import RenewYourHome from "@/components/organisms/RenewYourHome";
+import DownCategories from "@/components/organisms/DownCategoriesLinio";
 
 export default function Home() {
   return (
-    <><LinioMainCarrousel /><CategoriesLinio /> <LastSeen /> <DiscountsTeconology /> </>
+    <><LinioMainCarrousel /><CategoriesLinio /> <LastSeen /> <DiscountsTeconology /> <DontMissIt /> <RenewYourHome /> <DownCategories /></>
   );
 }
